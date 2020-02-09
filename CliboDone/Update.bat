06bc839e5270c38b511f03f8affccc98f46f136b
hideki.isobe <hidekiisobe9@gmail.com>
2020-02-09 15:53:33 +0900

    for Update.bat

---
+++ CliboDone/Update.bat
@@ -0,0 +1,42 @@
+@echo off
+
+rem Args check
+if "%~1" == "" (
+	exit /b 1
+)
+
+if "%~2" == "" (
+	exit /b 2
+)
+
+rem Variable declare
+set curPath=%~dp0
+
+set applicationDirPath=%~1
+set downloadDirPath=%~2
+set downloadApplicationDirPath=%~3
+
+set nowDate=%date:~0,4%%date:~5,2%%date:~8,2%
+set nowTime=%time:~0,2%%time:~3,2%%time:~6,2%
+
+rem Copy module files
+copy /b /v /y "%downloadApplicationDirPath%\*.exe" "%applicationDirPath%"
+copy /b /v /y "%downloadApplicationDirPath%\*.exe.config" "%applicationDirPath%"
+
+rem Copy dll files (If not exists dll then no check)
+copy /b /v /y "%downloadApplicationDirPath%\*.dll" "%applicationDirPath%"
+
+rem Copy manual
+rd /s /q "%applicationDirPath%\Manual"
+move /y "%downloadApplicationDirPath%\Manual" "%applicationDirPath%\Manual"
+
+rem Backup convert scripts
+robocopy /s /e "%applicationDirPath%\ConvertScripts" "%applicationDirPath%\ConvertScripts_bk_%date%%time%"
+
+rem Copy convert scripts (Newer or Changed)
+robocopy /s /e /xo "%downloadApplicationDirPath%\ConvertScripts" "%applicationDirPath%\ConvertScripts"
+
+rem Success
+exit /b 0
+
+@echo on
